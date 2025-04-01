ff99636792e05d25dcb49be388ceda1b76be5a26
devanshsingh05 <singhdewansh61@gmail.com>
2025-04-01 18:48:32 +0530

    card-size-update

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -6,13 +6,14 @@ function PostCard({$id, title, featuredImage}) {
     console.log("postcard se id : " + $id)
   return (
     <Link to={`/post/${$id}`}>
-      <div className="w-full bg-gray-100 rounded-xl p-4 border-1 border-black shadow-lg h-65">
+      <div className="w-full bg-gray-100 rounded-xl p-6 border-1 border-black shadow-lg h-65">
         <div className="w-full justify-center mb-4">
           <img
             src={appwriteService.getFilePreview(featuredImage)}
             alt={title}
             className="rounded-xl"
             width= "300px"
+            height= "300px"
           />
         </div>
         <h2 className="text-xl font-bold">{title}</h2>
